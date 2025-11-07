446a6c0ac17d2f14f4d02bacce867eeab762f7a5
Sebastião Victor <bysmuto0@gmail.com>
2025-11-07 15:22:36 -0300

    s t2

--- assets.js
+++ assets.js
@@ -3,7 +3,7 @@ let tickBuffer = null
 const gainNode = audioCtx.createGain()
 gainNode.gain.value = 0.2 
 
-fetch("sounds/tick.mp3")
+fetch("./sounds/tick.mp3")
   .then(res => res.arrayBuffer())
   .then(b => audioCtx.decodeAudioData(b))
   .then(buf => (tickBuffer = buf))
@@ -18,13 +18,13 @@ function playTick() {
 }
 
 
-const bgSound = new Audio("sounds/music.mp3");
-bgSound.volume = 0.05;
+const bgSound = new Audio("./sounds/music.mp3");
+bgSound.volume = 0.09;
 bgSound.loop = true;
 
-const preRollSound = new Audio("sounds/preRolL.mp3");
+const preRollSound = new Audio("./sounds/preRoll.mp3");
 preRollSound.volume = 0.4;
 
-const winSound = new Audio("sounds/win.mp3");
+const winSound = new Audio("./sounds/win.mp3");
 
 export { playTick, winSound, bgSound, preRollSound };
